cce2eddbcacfced64fbe89e44590318b549504d8
sserenitea <jeyang1@s.sfusd.edu>
2025-12-17 04:15:28 -0800

    L7 done

--- lessons/runner.gd
+++ lessons/runner.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @export var deacceleration := 1080
 
 @onready var _runner_visual: RunnerVisual = %RunnerVisualRed
+@onready var _dust: GPUParticles2D = %Dust
+
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left","move_right","move_up", "move_down")
@@ -25,5 +27,7 @@ func _physics_process(delta: float) -> void:
 			if current_speed_percent < 0.8
 			else RunnerVisual.Animations.RUN
 		)
+		_dust.emitting = true
 	else:
 		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		_dust.emitting = false
