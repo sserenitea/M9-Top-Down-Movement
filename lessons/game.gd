96482c72bf07011bd162ce1d1a9bfe06e2560e47
sserenitea <jeyang1@s.sfusd.edu>
2025-12-18 13:52:28 -0800

    L13 done

--- lessons/game.gd
+++ lessons/game.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var _finish_line: FinishLine = %FinishLine
 @onready var _count_down: CountDown = %CountDown
 @onready var _runner: Runner = %Runner
+@onready var _bouncer: CharacterBody2D = %Bouncer
+
 
 func _ready() -> void:
 	_finish_line.body_entered.connect(func (body: Node) -> void:
@@ -15,13 +17,11 @@ func _ready() -> void:
 			_finish_line.global_position
 			+ Vector2(0, 64)
 		)
-
 		runner.walk_to(destination_position)
 		runner.walked_to.connect(
 			_finish_line.pop_confettis
 		)
 	)
-
 	_finish_line.confettis_finished.connect(
 		get_tree().reload_current_scene.call_deferred
 	)
@@ -31,4 +31,11 @@ func _ready() -> void:
 		func() -> void:
 			_runner.set_physics_process(true)
 	)
+	
+	_bouncer.set_physics_process(false)
+	_count_down.counting_finished.connect(
+		func() -> void:
+			_bouncer.set_physics_process(true)
+	)
+	
 	
