7d2152bcb0d570a4bfacfc4bb8fce86d22dda773
sserenitea <jeyang1@s.sfusd.edu>
2025-12-18 16:00:35 -0800

    L14 & L15 done

--- lessons/bouncer.gd
+++ lessons/bouncer.gd
@@ -6,12 +6,24 @@ extends CharacterBody2D
 @onready var _runner_visual: RunnerVisual = %RunnerVisualPurple
 @onready var _dust: GPUParticles2D = %Dust
 @onready var _hit_box: Area2D = %HitBox
+@onready var _raycasts: Node2D = %Raycasts
+@export var avoidance_strength := 21000.0
 
+
+func _ready() -> void:
+	_hit_box.body_entered.connect(func(body: Node) -> void:
+		if body is Runner:
+			get_tree().reload_current_scene.call_deferred()
+	)
+	for raycast: RayCast2D in _raycasts.get_children():
+		raycast.add_exception(self)
+		
 func _physics_process(delta: float) -> void:
 	var direction := global_position.direction_to(get_global_player_position())
 	var distance := global_position.distance_to(get_global_player_position())
 	var speed := max_speed if distance > 100 else max_speed * distance / 100 
 	var desired_velocity := direction * max_speed
+	desired_velocity += calculate_avoidance_force() * delta
 	
 	velocity = velocity.move_toward(desired_velocity, acceleration * delta)
 	
@@ -19,6 +31,7 @@ func _physics_process(delta: float) -> void:
 	
 	if velocity.length() > 10:
 		_runner_visual.angle = rotate_toward(_runner_visual.angle, direction.orthogonal().angle(), 8 * delta)
+		_raycasts.rotation = _runner_visual.angle
 		var current_speed_percent := velocity.length() / max_speed
 		_runner_visual.animation_name = (
 			RunnerVisual.Animations.WALK
@@ -30,14 +43,20 @@ func _physics_process(delta: float) -> void:
 		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
 		_dust.emitting = false
 		
-		
 func get_global_player_position() -> Vector2:
 	return get_tree().root.get_node("Game/Runner").global_position
 		
-	
-func _ready() -> void:
-	_hit_box.body_entered.connect(func(body: Node) -> void:
-		if body is Runner:
-			get_tree().reload_current_scene.call_deferred()
-	)
-	
+func calculate_avoidance_force() -> Vector2:
+	var avoidance_force := Vector2.ZERO
+
+	for raycast: RayCast2D in _raycasts.get_children():
+		if raycast.is_colliding():
+			var collision_position := raycast.get_collision_point()
+			var direction_away_from_obstacle := collision_position.direction_to(raycast.global_position)
+			var ray_length := raycast.target_position.length()
+			var intensity := 1.0 - collision_position.distance_to(raycast.global_position) / ray_length
+
+			var force := direction_away_from_obstacle * avoidance_strength * intensity
+			avoidance_force += force
+
+	return avoidance_force
